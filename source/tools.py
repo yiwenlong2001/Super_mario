bec3c47fbb2ab5adf285001510861ae6e10e494c
culaccino <1136801871@qq.com>
2021-02-23 17:13:23 +0800

    super mario

--- source/tools.py
+++ source/tools.py
@@ -1,12 +1,13 @@
 import pygame
 import random
+import os
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.get_surface()
         self.clock = pygame.time.Clock()
 
-    def run(self):
+    def run(self,state):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -15,11 +16,26 @@ class Game:
                     self.keys = pygame.key.get_pressed()
                 elif event.type == pygame.KEYUP:
                     self.keys = pygame.key.get_pressed()
-            self.screen.fill((random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)))
+            state.update(self.screen)
             pygame.display.update()
             self.clock.tick(60)            
 
 def load_graph(path, accept=(' .jpg', ' .png', ' .bmp', ' .gif')):
     graphics = {}
     for pic in os.listdir(path):
-        name. ext
+        name, ext = os.path.splitext(pic)
+        if ext.lower() in accept:
+            img = pygame.image.load(os.path.join(path, pic))
+            if img.get_alpha():
+                img = img.convert_alpha()
+            else:
+                img = img.convert()
+            graphics[name] = img
+    return graphics
+
+def get_image(sheet, x, y, width, height, colorkey, scale):
+    image = pygame.surface((width,height))
+    image.blit(sheet, (0,0), (x, y, width, height))
+    image.set_corlorkey(colorkey)
+    image = pygame.transform.scale(image, (int(width*scale),int(height*scale)))
+    return image
