f5d6f3d4c030d3cb692cc9ff944e4a3045daa868
culaccino <1136801871@qq.com>
2021-02-23 16:14:58 +0800

    super_mario

---
+++ main.py
@@ -0,0 +1,10 @@
+#游戏主要入口
+import pygame
+from source import tools, setup
+
+def main():
+    game = tools.Game()
+    game.run()
+
+if __name__ ==  '__main__':
+    main()
---
+++ source/tools.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.get_surface()
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                elif event.type == pygame.KEYDOWN:
+                    self.keys = pygame.key.get_pressed()
+                elif event.type == pygame.KEYUP:
+                    self.keys = pygame.key.get_pressed()
+            self.screen.fill((random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)))
+            pygame.display.update()
+            self.clock.tick(60)            
+
+def load_graph(path, accept=(' .jpg', ' .png', ' .bmp', ' .gif')):
+    graphics = {}
+    for pic in os.listdir(path):
+        name. ext
